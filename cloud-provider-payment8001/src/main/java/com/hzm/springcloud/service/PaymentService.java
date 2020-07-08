5829bd945ed6f2c76850f6cd3ce2fab14736b2d2
huangzhenmu <huangzhenmu@ruixiude.com>
2020-07-08 23:31:24 +0800

    添加多配置的管理并在服务器上测试通过

--- cloud-provider-payment8001/src/main/java/com/hzm/springcloud/service/PaymentService.java
+++ cloud-provider-payment8001/src/main/java/com/hzm/springcloud/service/PaymentService.java
@@ -2,7 +2,6 @@ package com.hzm.springcloud.service;
 
 import com.hzm.springcloud.dao.PaymentDao;
 import com.hzm.springcloud.entity.Payment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
