5ba4e7cce9b71326bcbbf0a0361c334e76fe5b06
huangzhenmu <huangzhenmu@ruixiude.com>
2020-08-24 20:53:09 +0800

    gateway路由的两种配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/hzm/springcloud/GatewayMain9527.java
@@ -0,0 +1,18 @@
+package com.hzm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author hzm
+ * @version 1.0
+ * @date 2020/8/24 20:11
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
